f61c3ec84e28deeed6a125c049ade8ebe7ee0457
Ahemd <ahmedsaye23156@gmail.com>
2023-10-03 21:32:10 +0300

    new media

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+import IN from '@/pages/IN'
+
+export default function Home() {
+  return (
+  
+    <>
+      <h1>
+        {<IN/>}
+     </h1>
+    </>
+  )
+}
---
+++ src/pages/IN.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IN = () => {
+  return (
+    <h1>IN</h1>
+  )
+}
+
+export default IN
